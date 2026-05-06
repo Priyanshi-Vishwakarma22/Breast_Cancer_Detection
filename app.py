0c63213faae73fcfdcc310f4523f1a90af1e85a3
priyanshi Vishwakarma <156597759+Priyanshi-Vishwakarma22@users.noreply.github.com>
2026-05-06 13:30:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 import pandas as pd
 import sqlite3
 import pickle
